040ada5eeeaa75eb32e5398e26cca0032b0e719d
christopher <christopher.cop787@gmail.com>
2026-02-11 09:59:17 -0600

    feat: implement event edit and delete functionality with custom confirmation dialog and add development warning banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), node({ mode: 'standalone' })],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
